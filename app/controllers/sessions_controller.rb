49f0a1b36053d3be687af41c3281b731d791855a
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-04-24 19:56:00 +0300

    Add logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,13 @@ class SessionsController < ApplicationController
       log_in user
       remember user
     else
-
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
