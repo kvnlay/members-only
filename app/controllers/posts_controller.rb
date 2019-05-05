314415da2f2c8c32c04cd49d53f9e4dd94983242
kvnlay <kingstevens007@gmail.com>
2019-05-05 16:50:16 +0100

    refactored posts_controller#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
+    @post = current_user.posts.build(post_params)
 
     if @post.save
       flash[:success] = 'Post was successfully created'
