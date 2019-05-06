f7e74118d0db5b58b2bb935b5ff6e6fe857abf6d
kvnlay <kingstevens007@gmail.com>
2019-05-06 12:37:50 +0100

    Removed logic from view helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,42 +2,4 @@
 
 # Sessions Helper
 module SessionsHelper
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-
-  def remember(user)
-    remember_token = User.new_token
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-    user.update_attribute(:remember_digest, User.encrypt(remember_token))
-  end
-
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user&.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
-
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
-
-  def log_out
-    forget(current_user)
-    session.delete(:user_id)
-    @current_user = nil
-  end
-
-  def logged_in?
-    !current_user.nil?
-  end
 end
